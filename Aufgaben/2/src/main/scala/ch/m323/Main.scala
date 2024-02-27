70b371f3d634d8858f3c09c40dad6d674a9d4578
Sky <skybollin@frox.ch>
2024-02-27 07:54:10 +0100

    A1 finished

---
+++ Aufgaben/2/src/main/scala/ch/m323/Main.scala
@@ -0,0 +1,7 @@
+package ch.m323
+
+// Functions have been automatically changed to scala functions
+
+object Main extends App {
+  println("I love Scala!");
+}
---
+++ Aufgaben/2/src/main/scala/ch/m323/ShoppingCart.java
@@ -0,0 +1,5 @@
+package ch.m323;
+
+public class ShoppingCart {
+
+}
